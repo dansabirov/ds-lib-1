6aaf15a213b6700192e39c1949c083243fee02f6
Danil Sabirov <dansabirov@yahoo.com>
2023-08-15 14:52:42 +0300

    store

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,16 @@
-export function sayHello() {
-  console.log('hi')
+export type State = {
+  count: number;
+  name?: string;
+};
+
+let currentState: State = {
+  count: 0
+};
+
+export function setState(newState: State): void {
+  currentState = newState;
 }
-export function sayGoodbye() {
-  console.log('goodbye')
+
+export function getState(): State {
+  return currentState;
 }
